832af6b2b6ba858474f5dd0bf737fb37e9d0790c
cleverson <cleverson461@gmail.com>
2024-03-10 09:28:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log(3 > 2 && 'Cleverson' === 'Davi') // false
+console.log(3 > 2 && 'Davi' === 'Davi') // true
+console.log('------------')
+console.log(3 > 2 || 'Davi' === 'Dai') // true
+console.log(3 < 2 || 'Davi' === 'Dai') // true
+console.log('------------')
+
+console.log(!(3 < 2 || 'Davi' === 'Dai')) // true
+console.log(!(3 > 2 || 'Davi' === 'Dai')) // true
+
